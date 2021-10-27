629f97cbf2bf996d2accc7a1689da33a8742bb3b
rungsiyanon@gmail.com <rungsiyanon@gmail.com>
2021-10-27 14:46:45 +0700

    commint 1

--- pages/_document.js
+++ pages/_document.js
@@ -16,6 +16,14 @@ class MyDocument extends Document {
                         <meta name="viewport" content="width=device-width, initial-scale=1" />
                         <title>ระบบบริหารจัดการข้อมูล</title>
                         <div>
+                        {/*
+                            <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Prompt:wght@300&display=swap" />
+                            <link rel="stylesheet" href="plugins/fontawesome-free/css/all.min.css" />
+                            <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css" />
+                            <link rel="stylesheet" href="dist/css/adminlte.min.css" />
+                            <link rel="stylesheet" href="plugins/datatables-bs4/css/dataTables.bootstrap4.min.css" />
+                            <link rel="stylesheet" href="plugins/datatables-responsive/css/responsive.bootstrap4.min.css" />
+                            <link rel="stylesheet" href="plugins/datatables-buttons/css/buttons.bootstrap4.min.css" />
 
                             <Script type="text/javascript" src="plugins/datatables/jquery.dataTables.min.js"></Script>
                             <Script type="text/javascript" src="plugins/datatables-bs4/js/dataTables.bootstrap4.min.js"></Script>
@@ -30,6 +38,7 @@ class MyDocument extends Document {
                             <Script type="text/javascript" src="plugins/datatables-buttons/js/buttons.print.min.js"></Script>
                             <Script type="text/javascript" src="plugins/datatables-buttons/js/buttons.colVis.min.js"></Script>
                             <Script type="text/javascript" src="js/DataTable.js"></Script>
+                        */}
                         </div>
 
 
