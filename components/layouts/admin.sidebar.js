5725927f34ed4495a2c70c8af6fe4b879acc945b
aom01 <horjj440@gmail.com>
2021-11-03 11:52:29 +0700

    edit

--- components/layouts/admin.sidebar.js
+++ components/layouts/admin.sidebar.js
@@ -21,7 +21,7 @@ const AdminSidebar = () => {
       </div>
       <div className="info">
       <Link href='#'>
-        <a className="d-block">Mr.Kasidit Phosai</a>
+        <a className="d-block">Mr.Pathomporn Panyasermwut</a>
       </Link>
       </div>
     </div>
